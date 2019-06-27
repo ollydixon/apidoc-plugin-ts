c206915960368558fe1e3978a02ad8bf3535ee53
furstenheim <furstenheim@geoblink.com>
2019-06-27 16:01:04 +0200

    Fixes pr

--- test/test.ts
+++ test/test.ts
@@ -25,7 +25,10 @@ describe('Apidoc TS Plugin', () => {
     (test.only ? it.only : (test.skip ? it.skip : it))(test.name, async function () {
       const dest = `out/${test.name}`
       apidoc.createDoc({
-        src: `${test.name}`, debug: false, dest })
+        src: `${test.name}`,
+        debug: false,
+        dest
+      })
       const outputJson = await fs.readJson(path.join('out', test.name, 'api_data.json'))
       const expectedJson = await fs.readJson(path.join(test.name, 'fixture.json'))
       expect(outputJson).to.deep.equal(expectedJson)
